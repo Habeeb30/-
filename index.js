6273f469f42e9fdcf85bc0c816966e89fc0690d2
Habeebur Rahman <md.habeeb.che.bur@gmail.com>
2021-02-11 21:46:50 +0530

    Fix

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ fs.readdir("./commands/", (e, f) => {
     f.forEach(file => {
         if(!file.endsWith(".js")) return
         console.log(`${file} has been loaded`)
+        let cmd = require(`./commands/${file}`);
+        let cmdName = cmd.config.name;
+        client.commands.set(cmdName, cmd)
+        cmd.config.aliases.forEach(alias => {
+            client.aliases.set(alias, cmdName);
         })
     })
+})
+
+
 
 const queue = new Map();
 
