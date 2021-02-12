fe34c02203ef2451d43b262d0692fb2721494a90
Habeebur Rahman <md.habeeb.che.bur@gmail.com>
2021-02-12 13:31:26 +0530

    fix

--- commands/fskip.js
+++ commands/fskip.js
@@ -1,14 +1,14 @@
 module.exports.run = (client, message, args, queue, searcher) => {
     const serverQueue = queue.get(message.guild.id)
     if(message.member.voice.channel != message.guild.me.voice.channel)
-        return message.channel.send("Yᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ꜰɪʀꜱᴛ");
+        return message.channel.send("You need to join the voice chat first");
     if(!serverQueue)
-        return message.channel.send("ᴛʜᴇʀᴇ ɪꜱ ɴᴏᴛʜɪɴɢ ᴛᴏ ꜱᴋɪᴩ!");
+        return message.channel.send("There is nothing to skip!");
 
     let roleN = message.guild.roles.cache.find(role => role.name === "DJ")
 
     if(!message.member.roles.cache.get(roleN.id))
-        return message.channel.send("Yᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ᴅᴊ ʀᴏʟᴇ");
+        return message.channel.send("You don't have the DJ role");
 
     serverQueue.connection.dispatcher.end();
     serverQueue.skipVotes = [];
