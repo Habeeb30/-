69835156598f95f58eff99dba91865ffabce0cd7
Habeebur Rahman <md.habeeb.che.bur@gmail.com>
2021-02-12 13:44:05 +0530

    fix

--- commands/fskip.js
+++ commands/fskip.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args, queue, searcher) => {
-    const serverQueue = queue.get(message.guild)
+    const serverQueue = queue.get(message.guild.id)
     if(message.member.voice.channel != message.guild.me.voice.channel)
         return message.channel.send("You need to join the voice chat first");
     if(!serverQueue)
